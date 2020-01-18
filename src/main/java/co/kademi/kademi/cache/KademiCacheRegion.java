5e7ae34e47c605afb63c4241432e0f67eb26fd12
brad <brad@pop-os.localdomain>
2020-01-19 09:44:27 +1300

    kademi-cache 1.3.0 - reduce logging

--- src/main/java/co/kademi/kademi/cache/KademiCacheRegion.java
+++ src/main/java/co/kademi/kademi/cache/KademiCacheRegion.java
@@ -259,7 +259,7 @@ public abstract class KademiCacheRegion implements org.hibernate.cache.spi.Regio
             if (c.size() == 0) {
                 return;
             }
-            log.info("invalidateAll: cache: {} partition: {} current size={}", KademiCacheRegion.this.cacheName, partitionId, c.size());
+            //log.info("invalidateAll: cache: {} partition: {} current size={}", KademiCacheRegion.this.cacheName, partitionId, c.size());
             c.invalidateAll();
             //log.info("invalidateAll: partition: {} after invalidation size={}", partitionId, c.size());
             defaultCache.invalidateAll();
